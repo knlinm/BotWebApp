b92f0769b84c7c86850de1db84424ef85ab78651
knlinm <kenannuhbabali@gmail.com>
2024-07-02 05:11:26 +0400

    bugs fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
-document.querySelector(".tntCount").addEventListener("click",function(){
-    if(document.querySelector(".tntCount img").classList.contains("1")){
-        document.querySelector(".tntCount img").src=src="assets/img/2-removebg-preview.png"
+document.querySelector(".social").addEventListener("click", function () {
+    window.location.href = "http://www.w3schools.com";
+})
+
+document.querySelector(".tntCount").addEventListener("click", function () {
+    if (document.querySelector(".tntCount img").classList.contains("1")) {
+        document.querySelector(".tntCount img").src = src = "assets/img/2-removebg-preview.png"
         document.querySelector(".tntCount img").classList.remove("1")
         document.querySelector(".tntCount img").classList.add("2")
     }
-    else if(document.querySelector(".tntCount img").classList.contains("2")){
-        document.querySelector(".tntCount img").src=src="assets/img/1-removebg-preview.png"
+    else if (document.querySelector(".tntCount img").classList.contains("2")) {
+        document.querySelector(".tntCount img").src = src = "assets/img/1-removebg-preview.png"
         document.querySelector(".tntCount img").classList.remove("2")
         document.querySelector(".tntCount img").classList.add("1")
     }
-    
+
 })
-document.querySelector("img.button").addEventListener("click", function () {
-    if (document.querySelector(".item.opened") == null) {
+document.querySelector(".buttonWrapper").addEventListener("click", function () {
+    if (document.querySelector("p.counter").innerText == "Başlat") {
         allitems = document.querySelectorAll("div.item");
         countOfGold = [3, 4, 5];
         chance = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
@@ -55,12 +59,28 @@ document.querySelector("img.button").addEventListener("click", function () {
             allitems[places[3]].classList.add("opened")
             allitems[places[4]].classList.add("opened")
         }
+        document.querySelector("p.counter").innerText = "Sıfırla"
     }
-    else{
-       openedItems = document.querySelectorAll(".item.opened")
-       for(let i=0; i<openedItems.length;i++){
-        openedItems[i].classList.remove("opened")
-       }
+    else if (document.querySelector("p.counter").innerText == "Sıfırla") {
+        var timer = 59
+        var myTimer = setInterval(() => {
+            timer--
+            if (timer >= 10) {
+                document.querySelector("p.counter").innerText = "00:" + timer + ""
+            }
+            else{
+                document.querySelector("p.counter").innerText = "00:0" + timer + ""
+
+            }
+            if (timer == 0) {
+                document.querySelector("p.counter").innerText = "Başlat"
+                clearInterval(myTimer)
+            }
+        }, 1000);
+        openedItems = document.querySelectorAll(".item.opened")
+        for (let i = 0; i < openedItems.length; i++) {
+            openedItems[i].classList.remove("opened")
+        }
     }
 })
 
